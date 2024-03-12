ff746273d6bc418b6d344a5a9643d045b5d98518
Maor Barazani <maorba@monday.com>
2024-03-12 21:12:48 +0200

    add deep health endpoint

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
-const express = require('express');
-
+import express from 'express';
+import { Logger, SecureStorage } from '@mondaycom/apps-sdk';
 
 const app = express();
 const port = 8080;
 
 app.get('/health', (req, res) => {
-    res.status(200).send({"status": "OK"})
-})
+  res.status(200).send({ 'status': 'OK' });
+});
+
+app.get('/deepHealth', async (req, res) => {
+  const { randomWord } = req.query;
+  const secure = new SecureStorage();
+  const logger = new Logger('test-logger');
+
+  logger.info(`This is a logger info. the random word is: ${randomWord}`);
+
+  const key = Date.now() + '';
+  const value = 'test';
+
+  await secure.set(key, value);
+  const result = await secure.get(key);
+  const deleted = await secure.delete(key);
+  const result2 = await secure.get(key);
+
+  if (!(result === value && deleted && !result2)) {
+    res.status(500).send({ 'status': 'FAILED' });
+    return;
+  }
+
+  logger.debug(`Deep health finished successfully`);
+  res.status(200).send({ 'status': 'OK' });
+});
 
 
 app.listen(port, () => console.log(`monday code tester app listening at http://localhost:${port}`));
 
-module.exports = { app };
